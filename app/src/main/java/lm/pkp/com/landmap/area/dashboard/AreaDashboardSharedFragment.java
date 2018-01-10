1a6f4ae25d1ff1b3c0dde249a0f241b3eb7bf2b6
prasannapradhan <pradhans.prasanna@gmail.com>
2018-01-10 14:28:51 +0530

    Added combined area display for dashboard plotting directly.

--- app/src/main/java/lm/pkp/com/landmap/area/dashboard/AreaDashboardSharedFragment.java
+++ app/src/main/java/lm/pkp/com/landmap/area/dashboard/AreaDashboardSharedFragment.java
@@ -47,7 +47,7 @@ public class AreaDashboardSharedFragment extends Fragment
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(layout.fragment_shared_areas, container, false);
+        return inflater.inflate(R.layout.fragment_shared_areas, container, false);
     }
 
     @Override
